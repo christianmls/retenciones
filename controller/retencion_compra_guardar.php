f0c5c83bacccfc9bc297b809bb9a04650fad0216
LuchoNikoV <lucho.2012.tandil@gmail.com>
2018-02-22 13:25:33 -0300

    se hace el calculo de retenciones en el front end de las vistas, ahora falta implementar lo mismo en el backend

--- controller/retencion_compra_guardar.php
+++ controller/retencion_compra_guardar.php
@@ -20,6 +20,19 @@ class retencion_compra_guardar extends fs_controller
 
   protected function private_core()
   {
+    //obtenemos los codigos de retenciones
+    $this->reten   = $this->db->select('SELECT * FROM retenciones_sri WHERE tiporetencion = "renta"');
+    //obtenemos las lineas de factura correspondientes
+    if (!$this->is_assoc($_POST))
+      die('Parametros incorrectos');
+
+    $lineas = $_POST;
+    var_dump($lineas);
+  }
+
+  private function is_assoc($var)
+  {
+        return is_array($var) && array_diff_key($var,array_keys(array_keys($var)));
   }
 
 }
